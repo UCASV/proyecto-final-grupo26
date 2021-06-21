60b363906bf2dfed3cdd451fd346d7a5067d8547
KarenBurgos <00043820@uca.edu.sv>
2021-06-21 17:04:37 -0600

    Adding register form

---
+++ Programación orientada a objetos/Proyecto_POO_BDD/Proyecto_POO_BDD/frmRegisterCitizen.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Proyecto_POO_BDD
+{
+    public partial class frmRegisterCitizen : Form
+    {
+        public frmRegisterCitizen()
+        {
+            InitializeComponent();
+        }
+    }
+}
