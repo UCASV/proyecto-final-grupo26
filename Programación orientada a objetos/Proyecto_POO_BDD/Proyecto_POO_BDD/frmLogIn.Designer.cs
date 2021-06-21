26591ffc7feeaea1db0b9e5c844dc65632b5739e
KarenBurgos <00043820@uca.edu.sv>
2021-06-20 23:29:34 -0600

    adding frmLogIn

---
+++ Programación orientada a objetos/Proyecto_POO_BDD/Proyecto_POO_BDD/frmLogIn.Designer.cs
@@ -0,0 +1,165 @@
+﻿using System.ComponentModel;
+
+namespace Proyecto_POO_BDD
+{
+    partial class frmLogIn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogIn));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lbl_password = new System.Windows.Forms.Label();
+            this.lbl_user = new System.Windows.Forms.Label();
+            this.txt_user = new System.Windows.Forms.TextBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 32F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 58F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.Controls.Add(this.lbl_password, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.lbl_user, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txt_user, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txt_password, 2, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(287, 227);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // lbl_password
+            // 
+            this.lbl_password.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_password.Font = new System.Drawing.Font("Arial", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.lbl_password.Location = new System.Drawing.Point(17, 187);
+            this.lbl_password.Name = "lbl_password";
+            this.lbl_password.Size = new System.Drawing.Size(85, 40);
+            this.lbl_password.TabIndex = 2;
+            this.lbl_password.Text = "Contraseña:";
+            // 
+            // lbl_user
+            // 
+            this.lbl_user.Font = new System.Drawing.Font("Arial", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.lbl_user.Location = new System.Drawing.Point(17, 147);
+            this.lbl_user.Name = "lbl_user";
+            this.lbl_user.Size = new System.Drawing.Size(82, 40);
+            this.lbl_user.TabIndex = 1;
+            this.lbl_user.Text = "Usuario:";
+            // 
+            // txt_user
+            // 
+            this.txt_user.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_user.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.txt_user.Location = new System.Drawing.Point(108, 150);
+            this.txt_user.Name = "txt_user";
+            this.txt_user.Size = new System.Drawing.Size(143, 22);
+            this.txt_user.TabIndex = 3;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.tableLayoutPanel1.SetColumnSpan(this.pictureBox, 2);
+            this.pictureBox.Image = ((System.Drawing.Image) (resources.GetObject("pictureBox.Image")));
+            this.pictureBox.Location = new System.Drawing.Point(46, 3);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(193, 125);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // txt_password
+            // 
+            this.txt_password.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.txt_password.Location = new System.Drawing.Point(108, 190);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.PasswordChar = '*';
+            this.txt_password.Size = new System.Drawing.Size(143, 22);
+            this.txt_password.TabIndex = 4;
+            // 
+            // btn_login
+            // 
+            this.btn_login.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (237)))), ((int) (((byte) (27)))), ((int) (((byte) (36)))));
+            this.btn_login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_login.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.btn_login.ForeColor = System.Drawing.Color.White;
+            this.btn_login.Location = new System.Drawing.Point(17, 245);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(245, 35);
+            this.btn_login.TabIndex = 1;
+            this.btn_login.Text = "Iniciar sesión";
+            this.btn_login.UseVisualStyleBackColor = false;
+            // 
+            // frmLogIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(284, 313);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "frmLogIn";
+            this.Text = "Inicio de sesión";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button btn_login;
+
+        private System.Windows.Forms.Button button1;
+
+        private System.Windows.Forms.TextBox txt_password;
+
+        private System.Windows.Forms.TextBox txt_user;
+
+        private System.Windows.Forms.Label lbl_password;
+
+        private System.Windows.Forms.Label lbl_user;
+
+        private System.Windows.Forms.Label lbl_Password;
+
+        private System.Windows.Forms.PictureBox pictureBox;
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+
+        #endregion
+    }
+}
---
+++ Programación orientada a objetos/Proyecto_POO_BDD/Proyecto_POO_BDD/frmLogIn.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Proyecto_POO_BDD
+{
+    public partial class frmLogIn : Form
+    {
+        public frmLogIn()
+        {
+            InitializeComponent();
+        }
+    }
+}
