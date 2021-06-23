f30031f080fcf13eb6d36d36217e650d6ba6c8a9
edwinpalacios26 <54686508+edwinpalacios26@users.noreply.github.com>
2021-06-22 20:16:11 -0500

    Config Form

--- Programación orientada a objetos/Proyecto_POO_BDD/Proyecto_POO_BDD/frmLogIn.cs
+++ Programación orientada a objetos/Proyecto_POO_BDD/Proyecto_POO_BDD/frmLogIn.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using System.Windows.Forms;
+using Proyecto_POO_BDD.SqlServerContext;
 
 namespace Proyecto_POO_BDD
 {
@@ -12,11 +14,26 @@ namespace Proyecto_POO_BDD
 
         private void btn_login_Click(object sender, EventArgs e)
         {
+            var db = new ProCitasContext();
+            var listOfEmployees = db.Employees.
+                OrderBy(c => c.Id).ToList();
 
-            MessageBox.Show(
-                "Bienvenido", "Vaccination", MessageBoxButtons.OK,MessageBoxIcon.Hand);
+            var result = listOfEmployees.Where(e =>
+                e.NameEmployee.Equals(txt_user.Text) &&
+                e.Passwordd.Equals((txt_password.Text))).ToList();
 
-
+            if (result.Count == 0)
+                MessageBox.Show("Este usuario no existe", "Vaccination Program", MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation);
+            
+            else
+            {
+                MessageBox.Show("Bienvenido Gestor!", "Vaccination Program", MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+                frmMainMenu window = new frmMainMenu();
+                window.Show();
+                this.Hide();
+            }
         }
     }
 }
