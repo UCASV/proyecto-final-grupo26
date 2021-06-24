3b5bfa364af6ea2f7f0691ce242c0d2ec9aa8c54
KarenBurgos <00043820@uca.edu.sv>
2021-06-24 15:08:44 -0600

    modifing database

--- Bases de datos/ProCitas3.sql
+++ Bases de datos/ProCitas3.sql
@@ -17,7 +17,7 @@ CREATE TABLE CITIZEN(
 	id_cabin INT NOT NULL,
 	id_institution INT NULL,
 	id_direction INT NOT NULL,
-	id_info_vaccination INT,
+	id_info_vaccination INT NOT NULL,
 	id_side_effects INT Null,
 	id_diseases INT NULL
 );
@@ -26,10 +26,13 @@ CREATE TABLE INFO_VACCINATION(
 	id INT IDENTITY PRIMARY KEY,
 	date_appointment1 DATE,
 	time_appointment1 TIME,
-	vaccination_place VARCHAR(40),
-	date_appointment2 DATE,
-	time_appointment2 TIME
+	vaccination_place VARCHAR(40) NOT NULL,
+	date_appointment2 DATE NULL,
+	time_appointment2 TIME NULL
 );
+
+
+
 
 CREATE TABLE SIDE_EFFECTS(
 	id INT PRIMARY KEY,
@@ -55,7 +58,7 @@ CREATE TABLE EMPLOYEE(
 );
 
 CREATE TABLE RECORD(
-	id INT PRIMARY KEY,
+	id INT IDENTITY PRIMARY KEY,
 	dateR DATE,
 	timeR TIME
 );
@@ -244,6 +247,23 @@ INSERT INTO EMPLOYEE(id,nameEmployee,mail,id_direction,id_type_employee) VALUES(
 INSERT INTO EMPLOYEE(id,nameEmployee,mail,id_direction,id_type_employee) VALUES('27','Zohra Baez','zohra@gmail.com','50','1');
 INSERT INTO EMPLOYEE(id,nameEmployee,mail,id_direction,id_type_employee) VALUES('28','Angeles Orellana','angeles@gmail.com','54','1');
 
+--Banco de datos EMPLOYEEXCABIN
+INSERT INTO EMPLOYEEXCABIN(id_employee, id_cabin) VALUES('1','14');
+INSERT INTO EMPLOYEEXCABIN(id_employee, id_cabin) VALUES('2','13');
+INSERT INTO EMPLOYEEXCABIN(id_employee, id_cabin) VALUES('3','12');
+INSERT INTO EMPLOYEEXCABIN(id_employee, id_cabin) VALUES('4','11');
+INSERT INTO EMPLOYEEXCABIN(id_employee, id_cabin) VALUES('5','10');
+INSERT INTO EMPLOYEEXCABIN(id_employee, id_cabin) VALUES('6','9');
+INSERT INTO EMPLOYEEXCABIN(id_employee, id_cabin) VALUES('7','8');
+INSERT INTO EMPLOYEEXCABIN(id_employee, id_cabin) VALUES('8','7');
+INSERT INTO EMPLOYEEXCABIN(id_employee, id_cabin) VALUES('9','6');
+INSERT INTO EMPLOYEEXCABIN(id_employee, id_cabin) VALUES('10','5');
+INSERT INTO EMPLOYEEXCABIN(id_employee, id_cabin) VALUES('11','4');
+INSERT INTO EMPLOYEEXCABIN(id_employee, id_cabin) VALUES('12','3');
+INSERT INTO EMPLOYEEXCABIN(id_employee, id_cabin) VALUES('13','2');
+INSERT INTO EMPLOYEEXCABIN(id_employee, id_cabin) VALUES('14','1');
+
+
 --Banco de datos CITIZEN
 INSERT INTO CITIZEN(dui,nameCitizen,phone,mail,id_employee,id_cabin,id_institution,id_direction) VALUES('06579183-4','Julie Fleming','79968165','julie@gmail.com','5','1','2','1');
 INSERT INTO CITIZEN(dui,nameCitizen,phone,mail,id_employee,id_cabin,id_institution,id_direction) VALUES('05478211-5','Avye Wiley','76839474','avye@gmail.com','1','3','7','5');
@@ -260,7 +280,10 @@ INSERT INTO CITIZEN(dui,nameCitizen,phone,mail,id_employee,id_cabin,id_instituti
 INSERT INTO CITIZEN(dui,nameCitizen,phone,mail,id_employee,id_cabin,id_institution,id_direction) VALUES('02587488-9','Jason Cruz','76259774','jason@gmail.com','11','7','9','49');
 INSERT INTO CITIZEN(dui,nameCitizen,phone,mail,id_employee,id_cabin,id_institution,id_direction,id_diseases) VALUES('03258965-5','Alessandro Pineda','71335758','alessandro@gmail.com','13','12','5','53','3');
 
-
+--SELECT * FROM INFO_VACCINATION;
+--SELECT * FROM CITIZEN;
+--Select * FROM RECORD;
+--Select * FROM INSTITUTION;
 
 
 
